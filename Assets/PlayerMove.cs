9cf4d51be668def618edf70e64c363d9d9fb770e
20201287강민혁 <83389456+Land0116@users.noreply.github.com>
2025-03-07 01:06:34 +0900

    PlayerMove - forward

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,27 +1,52 @@
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 0f; // speed
-    Rigidbody m_rigidBody;
+    public float verticalVelocity = 0.0f;
+    public float gravity = 0.0f;
+    //Rigidbody m_rigidBody;
+    CharacterController m_characterController;
     Vector3 m_Movement;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        m_rigidBody = GetComponent<Rigidbody>();
-
+        //m_rigidBody = GetComponent<Rigidbody>();
+        m_characterController = GetComponent<CharacterController>();
+        gravity = 0.001f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        // float horizontal = Input.GetAxis("Horizontal");
+        // float vertical = Input.GetAxis("Vertical");
 
-        m_Movement = new Vector3(horizontal, 0, vertical).normalized;
+        // m_Movement = new Vector3(horizontal, 0, vertical).normalized;
 
-        transform.position += m_Movement * moveSpeed * Time.deltaTime;
+        // transform.position += m_Movement * moveSpeed * Time.deltaTime;
 
+    }
+
+    void FixedUpdate()
+    {
+        m_Movement = Vector3.zero;
+
+        if(m_characterController.isGrounded)
+        {
+            verticalVelocity = -0.2f;
+        }
+        else   
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        m_Movement.y = verticalVelocity;
+        m_Movement.z = moveSpeed * Time.deltaTime;
+        m_characterController.Move(m_Movement);
     }
 }
